bd26296ce00981dbf379ced44454d4f1408aed9b
Lluc Cardoner <lluccardoner@gmail.com>
2019-03-06 22:04:01 +0100

    data understanding notebook

--- src/baseline/baseline-DDI.py
+++ src/baseline/baseline-DDI.py
@@ -6,31 +6,34 @@ from os import listdir
 from xml.dom.minidom import parse
 from nltk.tokenize import word_tokenize
 
-effect_clues = ["administered", "concurrently", "concomitantly", "increase", "increases", "increased", "effect", "effects",  "prevent", "prevents", "prevented",  "potentiate", "potentiates", "potentiated"]
-mechanism_clues = [ "reduce", "reduces", "reduced", "decrease", "decreases", "decreased",  "change", "changes", "changed","elevate", "elevates", "elevated", "interfere", "interferes", "interfered"]
+effect_clues = ["administered", "concurrently", "concomitantly", "increase", "increases", "increased", "effect",
+                "effects", "prevent", "prevents", "prevented", "potentiate", "potentiates", "potentiated"]
+mechanism_clues = ["reduce", "reduces", "reduced", "decrease", "decreases", "decreased", "change", "changes", "changed",
+                   "elevate", "elevates", "elevated", "interfere", "interferes", "interfered"]
 
-## ------------------- 
+
+## -------------------
 ## -- check if a pair has an interaction, of which type
 
-def check_interaction(tokens,entities,e1,e2,pair) :
-   e1_end = int(entities[e1][-1])
-   e2_start = int(entities[e2][0])
-   
-   for t in tokens :
-      tk_word = t[0]
-      tk_start = t[1]
-      tk_end = t[2]
-      if tk_start > e1_end and tk_end < e2_start :
-         # the token is between both entities
-         # check if it is a word indicating interaction
-         if tk_word.lower() in effect_clues:
-            return "1","effect"
-         elif tk_word.lower() in mechanism_clues:
-            return "1","mechanism"
+def check_interaction(tokens, entities, e1, e2, pair):
+    e1_end = int(entities[e1][-1])
+    e2_start = int(entities[e2][0])
 
-   return "0","null"
+    for t in tokens:
+        tk_word = t[0]
+        tk_start = t[1]
+        tk_end = t[2]
+        if tk_start > e1_end and tk_end < e2_start:
+            # the token is between both entities
+            # check if it is a word indicating interaction
+            if tk_word.lower() in effect_clues:
+                return "1", "effect"
+            elif tk_word.lower() in mechanism_clues:
+                return "1", "mechanism"
 
-   
+    return "0", "null"
+
+
 ## --------- tokenize sentence ----------- 
 ## -- Tokenize sentence, returning tokens and span offsets
 
@@ -39,7 +42,7 @@ def tokenize(txt):
     tks = []
     for t in word_tokenize(txt):
         offset = txt.find(t, offset)
-        tks.append((t, offset, offset+len(t)-1))
+        tks.append((t, offset, offset + len(t) - 1))
         offset += len(t)
     return tks
 
@@ -56,33 +59,31 @@ def tokenize(txt):
 datadir = sys.argv[1]
 
 # process each file in directory
-for f in listdir(datadir) :
+for f in listdir(datadir):
 
     # parse XML file, obtaining a DOM tree
-    tree = parse(datadir+"/"+f)
+    tree = parse(datadir + "/" + f)
 
     # process each sentence in the file
     sentences = tree.getElementsByTagName("sentence")
-    for s in sentences :
-        sid = s.attributes["id"].value   # get sentence id
-        stext = s.attributes["text"].value   # get sentence text
+    for s in sentences:
+        sid = s.attributes["id"].value  # get sentence id
+        stext = s.attributes["text"].value  # get sentence text
 
         tokens = tokenize(stext)
-        
+
         # load sentence entities
         entities = {}
         ents = s.getElementsByTagName("entity")
-        for e in ents :
-           id = e.attributes["id"].value
-           offs = e.attributes["charOffset"].value.split("-")           
-           entities[id] = offs
-        
+        for e in ents:
+            id = e.attributes["id"].value
+            offs = e.attributes["charOffset"].value.split("-")
+            entities[id] = offs
+
         # for each pair in the sentence, decide whether it is DDI and its type
         pairs = s.getElementsByTagName("pair")
         for p in pairs:
-           id_e1 = p.attributes["e1"].value
-           id_e2 = p.attributes["e2"].value
-           (is_ddi,ddi_type) = check_interaction(tokens,entities,id_e1,id_e2,p) 
-           print(sid+"|"+id_e1+"|"+id_e2+"|"+is_ddi+"|"+ddi_type)
-
-
+            id_e1 = p.attributes["e1"].value
+            id_e2 = p.attributes["e2"].value
+            (is_ddi, ddi_type) = check_interaction(tokens, entities, id_e1, id_e2, p)
+            print(sid + "|" + id_e1 + "|" + id_e2 + "|" + is_ddi + "|" + ddi_type)
--- src/utils/dumpNER.py
+++ src/utils/dumpNER.py
@@ -8,12 +8,11 @@ from xml.dom.minidom import parse
 datadir = sys.argv[1]
 
 # process each file in directory
-for f in listdir(datadir) :
-   # parse XML file
-   tree = parse(datadir+"/"+f)
+for f in listdir(datadir):
+    # parse XML file
+    tree = parse(datadir + "/" + f)
 
-   # extract and print entities in file
-   entities = tree.getElementsByTagName("entity")
-   for e in entities :
-      print(e.attributes["text"].value, e.attributes["type"].value)
-
+    # extract and print entities in file
+    entities = tree.getElementsByTagName("entity")
+    for e in entities:
+        print(e.attributes["text"].value, e.attributes["type"].value)
