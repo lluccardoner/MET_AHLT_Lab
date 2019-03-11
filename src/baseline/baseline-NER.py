486c57c3633594dc087144b163ce4025650b6718
Lluc Cardoner <lluccardoner@gmail.com>
2019-03-12 00:29:40 +0100

    entity tree classification

--- src/baseline/baseline-NER.py
+++ src/baseline/baseline-NER.py
@@ -20,6 +20,18 @@ suffixes = ["idase", "idone", "uride", "ogens", "rinol", "amate", "lones", "pami
             "mycin", "idine", "amine", "azole"]
 
 
+def load_suffixes_5():
+    loaded = {}
+    for k in ["brand", "drug", "group", "drug_n"]:
+        with open('/Users/lluccardoner/PycharmProjects/MET_AHLT_Lab_2019/src/data/' + k + '_suffix_5_DrugBank.txt',
+                  'r') as f:
+            loaded[k] = f.read().splitlines()
+    return loaded
+
+
+type_suffixes = load_suffixes_5()
+
+
 def classify_token(txt):
     if txt.isupper():
         return True, "brand"
@@ -29,7 +41,31 @@ def classify_token(txt):
         return False, ""
 
 
-## --------- tokenize sentence ----------- 
+def classify_token_2(txt):
+    if txt.isupper():
+        return True, "brand"
+    elif txt[-5:] in suffixes:
+        return True, "drug"
+    elif txt[-1:] == "s":
+        return True, "group"
+    else:
+        return False, ""
+
+
+def classify_token_3(txt):
+    if txt.isupper() or txt[-5:].lower() in type_suffixes["brand"]:
+        return True, "brand"
+    elif txt[-5:].lower() in type_suffixes["drug"]:
+        return True, "drug"
+    elif txt[-5:].lower() in type_suffixes["group"]:
+        return True, "group"
+    elif txt[-5:].lower() in type_suffixes["drug_n"]:
+        return True, "drug_n"
+    else:
+        return False, ""
+
+
+## --------- tokenize sentence -----------
 ## -- Tokenize sentence, returning tokens and span offsets
 
 def tokenize(txt):
@@ -37,7 +73,7 @@ def tokenize(txt):
     tks = []
     for t in word_tokenize(txt):
         offset = txt.find(t, offset)
-        tks.append((t, offset, offset + len(t) - 1))
+        tks.append((t, offset, offset + len(t)))
         offset += len(t)
     return tks
 
@@ -46,14 +82,23 @@ def tokenize(txt):
 ## -- Extract drug entities from given text and return them as
 ## -- a list of dictionaries with keys "offset", "text", and "type"
 
-def extract_entities(stext):
+def extract_entities(stext, classifier="1"):
     result = []
     tokens = tokenize(stext)
 
     prev_drug = False
     for t in tokens:
         tokenTxt = t[0]
-        (is_drug, tk_type) = classify_token(tokenTxt)
+
+        if classifier == "1":
+            (is_drug, tk_type) = classify_token(tokenTxt)
+        elif classifier == "2":
+            (is_drug, tk_type) = classify_token_2(tokenTxt)
+        elif classifier == "3":
+            (is_drug, tk_type) = classify_token_3(tokenTxt)
+        else:
+            print("No classifier", classifier)
+            exit(1)
 
         if not is_drug and not prev_drug:
             # non-drug token after a non-drug token.
@@ -96,6 +141,7 @@ def extract_entities(stext):
 
 # directory with files to process
 datadir = sys.argv[1]
+classifier = sys.argv[2]
 
 # process each file in directory
 for f in listdir(datadir):
@@ -110,7 +156,7 @@ for f in listdir(datadir):
         stext = s.attributes["text"].value  # get sentence text
 
         # extract entities in text
-        entities = extract_entities(stext)
+        entities = extract_entities(stext, classifier)
 
         # print sentence entities in format requested for evaluation
         for e in entities:
